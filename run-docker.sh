65b6218c427362d5b295748a2aaa0b13cad63e50
Piotr Lucinski <lootek87@gmail.com>
2018-02-10 20:48:18 +0100

    run-docker: added scurity switches for delve

--- run-docker.sh
+++ run-docker.sh
@@ -6,6 +6,8 @@ docker run \
 	--rm \
 	--publish 8000:80 \
 	--publish 8008:8333 \
+	--security-opt=seccomp:unconfined \
+	--cap-add=SYS_PTRACE \
 	--volume "$PWD":/go/src/go-training \
 	--workdir /go/src/go-training \
 	go-training
