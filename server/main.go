3a4f9d12309a336034fe4754e8d720f617495f3e
lootek <piotr.lucinski@solarwinds.cloud>
2018-02-12 12:00:26 +0100

    server: now capable of writing JSON and XML

--- server/main.go
+++ server/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/lootek/go-training/server/storage"
@@ -22,7 +24,13 @@ func datastoreHandle(w http.ResponseWriter, r *http.Request) {
 			Value int    `json:"v"`
 		}{}
 
-		json.NewDecoder(r.Body).Decode(&item)
+		err := json.NewDecoder(r.Body).Decode(&item)
+		if err != nil {
+			log.Printf("cannot decode json: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		fmt.Printf("%#v\n", item)
 
 		storage.Save(item.Key, item.Value)
@@ -36,8 +44,33 @@ func datastoreHandle(w http.ResponseWriter, r *http.Request) {
 		}
 
 		fmt.Printf("%#v\n", values)
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		json.NewEncoder(w).Encode(&values)
+
+		switch r.URL.Query().Get("format") {
+		case "xml":
+			data, err := xml.Marshal(&values)
+			if err != nil {
+				log.Printf("cannot encode json: %v", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+			w.Write(data)
+		case "json":
+			fallthrough
+		default:
+			data, err := json.Marshal(&values)
+			if err != nil {
+				log.Printf("cannot encode json: %v", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			w.Write(data)
+		}
 	}
 }
 
